3bd1df593a4b150674d7114695b179205b7c8efc
Valeriia Lyakisheva <valeriia.lyakisheva@bahag.com>
2024-11-20 10:35:28 +0100

    trying adjustment

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config: { module: { rules: { test: RegExp; use: string }[] } }) {
+    config.module.rules.push({
+      test: /\.tests\.tsx?$/,
+      use: "ignore-loader", // Ignore test files during production build
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
